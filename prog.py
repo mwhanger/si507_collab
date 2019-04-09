b4b0a889e0bee0168598e195f538a168982afac1
Matt Hanger <mhanger@umich.edu>
2019-04-09 11:19:45 -0400

    Updates and comments to file

--- prog.py
+++ prog.py
@@ -1,7 +1,11 @@
 import random
+import json
 
 def num_vowels(string):
+    #count starts at 0
     count = 0
+    #looks at each character to see if it's in vowel string
     for c in string:
+        #if it is, add 1 to counter
         if c in 'aeiou':
             count += 1
